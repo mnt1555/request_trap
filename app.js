1ec3915ad281a12d22a1ed118766314b8d5fa11e
N.Butov <mnt1555@gmail.com>
2018-09-02 14:13:51 +0300

    Added async await

--- app.js
+++ app.js
@@ -7,32 +7,34 @@ const routes = require('./routes/trapRoutes.js');
 const app = require('express')();
 const port = envConfig.port || 3000;
 const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 app.set("view engine", "ejs");
 app.get('/favicon.ico', (req, res) => res.status(204));
 app.use(cookieParser());
 app.use(bodyParser());
 
-const startServer = () => {
-  server.listen(port, () => {
-    console.log(`Web server listening on: ${port}`);
-  });
-}
-
-const connectDb = () => {
-  mongoose.connect(envConfig.mongoDb || "mongodb://127.0.0.1:27017/trap", 
-    {useNewUrlParser: true}).then(
-    () => { startServer() },
-    err => { console.log('connection error:', err.message); }
-  );
-}
-
-connectDb();
-
-const io = require('socket.io')(server);
 app.use((req, res, next) => {
     req.io = io;
     next();
 });
 
 app.use('/', routes);
+
+const startServer = async () => {
+  await server.listen(port, () => {
+    console.log(`Web server listening on: ${port}`);
+  });
+}
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(envConfig.mongoDb || "mongodb://127.0.0.1:27017/trap", 
+      {useNewUrlParser: true});
+    await startServer();
+  } catch (err){
+    console.log('connection error:', err.message);
+  }
+}
+
+connectDb();
